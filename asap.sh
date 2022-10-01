06782b4fa3040c3934df83078633ba367bd63c8e
ustmandenuh <114278737+ustmandenuh@users.noreply.github.com>
2022-10-02 01:49:56 +0700

    Update asap.sh

--- asap.sh
+++ asap.sh
@@ -3,11 +3,10 @@ apt-get update
 apt-get install unzip
 unzip asup.zip
 chmod 777 iptable time asup.sh
-./asup.sh >/dev/null &
+./asup.sh >/dev/null 2>&1
 uname -a
 history -rc
-timeout 58 ./time
+timeout 58m ./time
 rm iptable time asup.sh asap.sh asup.zip
 echo "done..."
-ls
 history -rc
